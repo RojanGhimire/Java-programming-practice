6671cd64ff53f0ca91b57242325a7ccb304d7143
RojanGhimire <ghimirerojan41@gmail.com>
2026-06-11 06:55:02 +0545

    commit june 11

---
+++ Test.java
@@ -0,0 +1,25 @@
+class Animal
+{
+
+}
+
+class Dog extends Animal
+{
+    
+}
+
+class Cat 
+{
+    
+}
+public class Test
+{
+public static void main(String[] args) {
+    
+
+Dog d=new Dog();
+Animal a=new Animal();
+
+System.out.println(d instanceof Dog);
+System.out.println(a instanceof Dog);
+}}
